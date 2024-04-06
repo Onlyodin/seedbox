d724b65c02b764de2115000a3e1d9c31edccb77a
Onlyodin <onlyodin@xsnet.org>
2024-04-06 22:47:59 +1100

    Improved formatting, added Munin graphs

--- html/index.php
+++ html/index.php
@@ -24,19 +24,72 @@
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: cover;
+            font-family: "Inter", Helvetica, sans-serif;
+        }
+        h4 {
+            margin: 0 0;
         }
     </style>
-    <?php
-        $text = gethostname();
-        $figletOutput = shell_exec("figlet '$text'");
-        echo "<pre>$figletOutput</pre>";
-    ?>
-    <p>Welcome, <?php echo $user; ?></p><br />
-    <p>Here is your slot information:</p>
-    <p>Total Slot Size: <?php echo formatBytes(shell_exec("lsblk -b --output SIZE -n $homeLvm")); ?></p>
-    <p>Available Space: <?php echo formatBytes(disk_free_space($homeDirectory)); ?></p><br />
-    <p>Quick Links:</p>
-    <p><a href="/rutorrent">ruTorrent Interface</a></p>
-    <p>Not much else to see here yet. Come back soon.</p>
+    <div style="width: 90%; margin: 0 auto; display: flex;">
+    <div>
+        <div>
+            <?php
+                $text = gethostname();
+                $figletOutput = shell_exec("figlet '$text'");
+                echo "\n<pre>\n" . $figletOutput . "</pre>\n";
+            ?>
+        </div>
+        <div>
+            <h2>Welcome, <?php echo ucfirst($user); ?>!</h2><br />
+        </div>
+        <div style="display: flex;">
+            <div style="width: 50%;">
+                    <!-- Content for the first div -->
+                    <h4>Slot information:</h4>
+                    <div style="display: flex;">
+                        <div style="width: 50%;">
+                            <p>Total Slot Size:</p>
+                            <p>Available Space:</p>
+                        </div>
+                        <div style="width: 50%;">
+                            <?php
+                            echo "<p>" . formatBytes(shell_exec("lsblk -b --output SIZE -n $homeLvm")) . "</p>";
+                            echo "<p>" . formatBytes(disk_free_space($homeDirectory)) . "</p>";
+                            ?>
+                        </div>
+                    </div>
+                    <br />
+            </div>
+            <div style="width: 50%;">
+                <!-- Content for the second div -->
+                <h4>Quick Links:</h4>
+                <div style="display: flex;">
+                    <div>
+                        <p><a href="/rutorrent">ruTorrent Interface</a></p>
+                        <p><a href="/<?php echo $user; ?>">Downloads directory</a></p>
+                        <br /><br />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+        <h3>rTorrent Statistics</h3>
+            <?php
+            $fqdn = $_SERVER['SERVER_NAME'];
+            $domain = str_replace(gethostname() . '.', '', $_SERVER['SERVER_NAME']);
+            $imagePath = "/munin/" . $domain . "/" . $fqdn . "/rtom_";
+            $rtomPlugins = array("spdd", "vol", "peers", "mem");
+            foreach ($rtomPlugins as $plugin) {
+                echo "\n    <p>\n";
+                echo "        <img src=\"" . $imagePath . $user . "_" . $plugin . "-day.png\" alt=\"rTorrent " . $plugin . " - by day\">\n";
+                echo "        <img src=\"" . $imagePath . $user . "_" . $plugin . "-week.png\" alt=\"rTorrent " . $plugin . " - by week\">\n";
+                echo "    </p>";
+            }
+            echo "\n";
+            ?>    
+        </div>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
